5be25baff0a2a8661ef4a66be2ba3324cc1523b5
kbuzcu <kbuzcu@kazandirio-dev.com>
2021-07-11 13:20:23 +0300

    done tweetform

--- src/component/tweetForm/tweetForm.js
+++ src/component/tweetForm/tweetForm.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Icon } from '../icon';
 
-const TweetForm = () => {
+const TweetForm = (props) => {
+    const { tweetText, onChangeTweetForm, handleTweetSubmit } = props;
+
     return (
         <div className="tweetField">
             <div className="tweetField__profilePic">
                 <Icon size={46} iconName="user" />
             </div>
             <div className="tweetField__inputContainer">
-                <div
+                <input
                     className="tweetField__input"
-                    contentEditable="true">
-                    What's happening?
-                </div>
+                    onChange={onChangeTweetForm}
+                    placeholder="What's Happening"
+                    value={tweetText}
+                />
                 <div className="tweetField__options">
                     <div className="tweetField__tools">
                         <button className="tweetField__items">
@@ -23,7 +26,7 @@ const TweetForm = () => {
                         </button>
                     </div>
                     <div className="tweetField__submit">
-                        <button className="tweetField__submitButton">
+                        <button onClick={handleTweetSubmit} className="tweetField__submitButton">
                             Tweet
                         </button>
                     </div>
--- src/view/home.js
+++ src/view/home.js
@@ -3,11 +3,33 @@ import { Header } from '../container';
 import { TweetForm } from '../component/tweetForm';
 
 class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            tweetText: ""
+        }
+        this.onChangeTweetForm = this.onChangeTweetForm.bind(this);
+        this.handleTweetSubmit = this.handleTweetSubmit.bind(this);
+    }
+
+    onChangeTweetForm(event) {
+        this.setState({ tweetText: event.target.value });
+    }
+
+    handleTweetSubmit() {
+        console.log(this.state.tweetText);
+    }
+
     render() {
+        const { tweetText } = this.state;
+
         return (
             <div className="latestTweets">
                 <Header title="Home" />
-                <TweetForm />
+                <TweetForm
+                    tweetText={tweetText}
+                    onChangeTweetForm={this.onChangeTweetForm}
+                    handleTweetSubmit={this.handleTweetSubmit} />
                 <div className="latestTweets__divisor" />
                 <span>asgadsf</span>
             </div>
